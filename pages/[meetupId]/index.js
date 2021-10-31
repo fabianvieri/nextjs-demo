a7e09caefd34736d155ebe93859256ab3c45da68
fabianvieri <vierifabian@gmail.com>
2021-10-31 19:15:27 +0700

    adjust revalidate value & fix fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -33,7 +33,7 @@ export async function getStaticPaths() {
   const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
